ba0534d210e0d4d4a3b637ef51a6a0806985784c
Todor Cholakov <t.s.cholakov@gmail.com>
2023-04-26 14:29:44 +0300

    lots of changes

--- wp-content/themes/softuni-jobs/index.php
+++ wp-content/themes/softuni-jobs/index.php
@@ -1,43 +1,11 @@
-<?php get_header() ?>
+<?php get_header(); ?>
+<?php if (have_posts()):?>
+	<ul class="jobs-listing">
+	<?php while(have_posts()): the_post(); ?>
+	
+		<?php get_template_part('template-parts/jobitem'); ?>
 
-		<ul class="jobs-listing">
-			<li class="job-card">
-				<div class="job-primary">
-					<h2 class="job-title"><a href="#">Front End Developer</a></h2>
-					<div class="job-meta">
-						<a class="meta-company" href="#">Company Awesome Ltd.</a>
-						<span class="meta-date">Posted 14 days ago</span>
-					</div>
-					<div class="job-details">
-						<span class="job-location">The Hague (The Netherlands)</span>
-						<span class="job-type">Contract staff</span>
-					</div>
-				</div>
-				<div class="job-logo">
-					<div class="job-logo-box">
-						<img src="https://i.imgur.com/ZbILm3F.png" alt="">
-					</div>
-				</div>
-			</li>
-
-			<li class="job-card">
-				<div class="job-primary">
-					<h2 class="job-title"><a href="#">Front End Developer</a></h2>
-					<div class="job-meta">
-						<a class="meta-company" href="#">Company Awesome Ltd.</a>
-						<span class="meta-date">Posted 14 days ago</span>
-					</div>
-					<div class="job-details">
-						<span class="job-location">The Hague (The Netherlands)</span>
-						<span class="job-type">Contract staff</span>
-					</div>
-				</div>
-				<div class="job-logo">
-					<div class="job-logo-box">
-						<img src="https://i.imgur.com/ZbILm3F.png" alt="">
-					</div>
-				</div>
-			</li>
-		</ul>
-		
-	<?php get_footer(); ?>
+	<?php endwhile; ?>
+<?php endif; ?>
+</ul>
+<?php get_footer(); ?>
